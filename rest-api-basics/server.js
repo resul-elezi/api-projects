5f40da57fc5242671975cf71d203aa6f168e72c0
re-d-e-v <resul_elezi@protonmail.com>
2023-11-01 14:45:43 +0100

    #

--- rest-api-basics/server.js
+++ rest-api-basics/server.js
@@ -4,4 +4,11 @@ const CORS = require("cors");
 const APP = EXPRESS();
 const PORT = process.env.PORT || 3000;
 
+APP.use(CORS());
+
+APP.options("*", (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+});
+
 APP.listen(PORT, () => console.log(`Listening on ${PORT}`));
