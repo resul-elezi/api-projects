c629c11867eff90b380548701187732e0c2cc802
re-d-e-v <resul_elezi@protonmail.com>
2023-11-01 15:03:29 +0100

    #

--- rest-api-basics/server.js
+++ rest-api-basics/server.js
@@ -10,6 +10,17 @@ APP.options("*", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-Requested-With");
+    res.send(200);
+});
+
+APP.use(EXPRESS.json());
+APP.use(EXPRESS.urlencoded({ extended: false}));
+APP.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+APP.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 APP.listen(PORT, () => console.log(`Listening on ${PORT}`));
