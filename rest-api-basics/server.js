36f8c411f09a844b91fa09c30ea4ff74bd03f171
re-d-e-v <resul_elezi@protonmail.com>
2023-11-01 14:38:17 +0100

    #

--- rest-api-basics/server.js
+++ rest-api-basics/server.js
@@ -1,7 +1,7 @@
 const EXPRESS = require("express");
 const CORS = require("cors");
 
-const APP = express();
+const APP = EXPRESS();
 const PORT = process.env.PORT || 3000;
 
 APP.listen(PORT, () => console.log(`Listening on ${PORT}`));
