d75ae32392460dea50cbfaf2213347d2bf0825f6
re-d-e-v <resul_elezi@protonmail.com>
2023-11-09 11:25:01 +0100

    added time.js

---
+++ rest-api-basics/routes/time.js
@@ -0,0 +1,11 @@
+const EXPRESS  = require("express");
+const ROUTER = EXPRESS.Router();
+
+ROUTER.get("/", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}), (req, res) => {
+    res.json({ "time": req.time });
+}
+
+module.exports = ROUTER; 
