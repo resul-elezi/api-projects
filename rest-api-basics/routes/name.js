53ba17519dba4537e5bc861d7155a699abcde259
Resul Elezi <resul_elezi@protonmail.com>
2023-11-11 14:22:59 +0100

    modified: name.js

--- rest-api-basics/routes/name.js
+++ rest-api-basics/routes/name.js
@@ -9,4 +9,23 @@ function nameReply(req, res) {
     const METHOD = req.METHOD;
     const FIRST_NAME = METHOD === "GET" ? req.query.first : req.body.first;
     const LAST_NAME = METHOD === "GET" ? req.query.last : req.body.last;
+
+    res.format({
+        "text/plain": () => {
+            res.send(`name: ${FIRST_NAME} ${LAST_NAME}`)
+        },
+        "text/html": () => {
+            let html = "<ul>";
+            html += `<li>name: ${FIRST_NAME} ${LAST_NAME}</li>`;
+            html += `</ul>`;
+            res.send(html);
+        },
+        "application/json": () => {
+            res.json({"name": `${FIRST_NAME} ${LAST_NAME}`});
+        },
+        "default": () => {
+            res.status(406).send("Not Acceptable");
+        }
+    });
 }
+module.exports = ROUTER;
